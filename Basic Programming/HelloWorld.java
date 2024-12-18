16cd33b16be06add59e54ee6b7c52aa809bc4f38
Merikh <krishnendunandy43@gmail.com>
2024-12-18 21:58:15 +0530

    Added Comments

--- Basic Programming/HelloWorld.java
+++ Basic Programming/HelloWorld.java
@@ -1,6 +1,8 @@
 import java.util.*;
-public class HelloWorld {
+//package com.company;  // A package in Java is used to group related classes. Think of it as a folder in a file directory.
+public class HelloWorld { // As file name is HelloWorld, the name of the class is also HelloWorld
     public static void main(String[] args) {
+        // Write your code here
         System.out.println("Hello World");
     }
 }
